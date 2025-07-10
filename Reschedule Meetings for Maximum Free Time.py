3234482898bb33b46cc326a65923074ea0f72ef8
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-10 13:07:05 +0530

    Update Reschedule Meetings for Maximum Free Time.py

--- Reschedule Meetings for Maximum Free Time.py
+++ Reschedule Meetings for Maximum Free Time.py
@@ -1,1 +1,19 @@
+class Solution:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        size = len(startTime)
+        gapsArr,left = [],0
+        for i in range(size):
+            gap = startTime[i] - left
+            gapsArr.append(gap)
+            left = endTime[i]
+        gapsArr.append(eventTime - endTime[-1])
+        maxGapPrefix,maxGapSuffix = [0 for i in range(size)],[0 for i in range(size)]
+        maxGapPrefix[0] = gapsArr[0]
+        maxGapSuffix[size-1] = gapsArr[-1]
 
+        # calculate left right max
+        for i in range(1,size):
+            maxGapPrefix[i]= max(maxGapPrefix[i-1],gapsArr[i])
+        for i in range(size-1,0,-1):
+            maxGapSuffix[i-1]= max(maxGapSuffix[i],gapsArr[i])
+          
