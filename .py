3375a60441583ee37725c053362c6ffe2150cafc
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-18 23:13:58 +0530

    Update .py

--- .py
+++ .py
@@ -1,3 +1,13 @@
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
+        n=len(nums)//3
+        diff=[0]*(n+1)
+        heapify(pqL:=[-x for x in nums[:n]])
+        heapify(pqR:=nums[2*n:])
+        Sum=sum(nums[:n])
+        ans=Sum
+        for i in range(n, 2*n+1):
+            diff[i-n]=Sum
+            x=nums[i]
+            
         
