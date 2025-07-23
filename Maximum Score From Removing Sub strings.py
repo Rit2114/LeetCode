ef77095ab726846eec30b0fb424c8bb797fc81b1
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-23 22:18:04 +0530

    Update Maximum Score From Removing Sub strings.py

--- Maximum Score From Removing Sub strings.py
+++ Maximum Score From Removing Sub strings.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        
+        def solve(text: str, pattern: str, score: int):
+            stack = []
+            points = 0
+            for char in text:
+                if stack and stack[-1] == pattern[0] and char == pattern[1]:
+                    stack.pop()
+                    points += score
+                else:
+                    stack.append(char)
+            return points, "".join(stack)
