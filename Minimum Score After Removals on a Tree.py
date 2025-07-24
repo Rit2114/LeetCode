4a8d52600e0b009f9e90c1ac065080914bbfa1d0
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-24 23:54:45 +0530

    Update Minimum Score After Removals on a Tree.py

--- Minimum Score After Removals on a Tree.py
+++ Minimum Score After Removals on a Tree.py
@@ -4,3 +4,8 @@ class Solution:
      for u,v in E:g[u]+=[v];g[v]+=[u]
      s=[0]*n;d=[0]*n
      def D(u,p):
+      s[u],d[u]=N[u],1<<u
+      for v in g[u]:
+       if v-p:D(v,u);s[u]^=s[v];d[u]|=d[v]
+     D(0,-1)
+     r,t=9e9,s[0]
