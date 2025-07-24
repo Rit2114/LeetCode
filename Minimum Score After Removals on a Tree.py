92cc2292fd2f6d528751778a7f9d1c944573e77b
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-24 23:53:59 +0530

    Update Minimum Score After Removals on a Tree.py

--- Minimum Score After Removals on a Tree.py
+++ Minimum Score After Removals on a Tree.py
@@ -1,3 +1,6 @@
 class Solution:
     def minimumScore(self, nums: List[int], edges: List[List[int]]) -> int:
-        
+      n=len(N);g=[[]for _ in N]
+     for u,v in E:g[u]+=[v];g[v]+=[u]
+     s=[0]*n;d=[0]*n
+     def D(u,p):
