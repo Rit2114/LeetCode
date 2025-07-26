e6a46b6a9f7a79d5a17233854be99d4ba68de0eb
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-26 17:37:57 +0530

    Update Maximize Subarrays After Removing One Conflicting Pair.py

--- Maximize Subarrays After Removing One Conflicting Pair.py
+++ Maximize Subarrays After Removing One Conflicting Pair.py
@@ -1,3 +1,6 @@
 class Solution:
     def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
+        right = [[] for _ in range(N + 1)]
+        for a, b in A:
+            right[max(a, b)].append(min(a, b))
         
