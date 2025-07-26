c61044bc5c3a507f59369d504120c905c092e082
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-26 17:38:23 +0530

    Update Maximize Subarrays After Removing One Conflicting Pair.py

--- Maximize Subarrays After Removing One Conflicting Pair.py
+++ Maximize Subarrays After Removing One Conflicting Pair.py
@@ -3,4 +3,8 @@ class Solution:
         right = [[] for _ in range(N + 1)]
         for a, b in A:
             right[max(a, b)].append(min(a, b))
+
+        ans = 0
+        left = [0, 0]
+        imp = [0] * (N + 1)
         
