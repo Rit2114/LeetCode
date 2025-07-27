05a35c43f65b5a0ee1b5f7316c35e065c8a2be5c
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-27 22:29:41 +0530

    Update .py

--- .py
+++ .py
@@ -3,3 +3,7 @@ class Solution:
         n, prev, cnt=len(nums), nums[0], 0
         diff=[0, 0]
         i=0
+        while i<n:
+            while i<n and prev==nums[i]: i+=1
+            if i==n: break
+            bigger=1 if nums[i]>prev 
