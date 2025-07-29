65d45d38d496d843901bf206c33bcb9eebd9ba0d
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-29 19:30:39 +0530

    Update .py

--- .py
+++ .py
@@ -6,3 +6,10 @@ class Solution:
             x = nums[i]
             answer[i] = 1
             j = i - 1
+            while j >= 0 and nums[j] | x != nums[j]:
+                answer[j] = i - j + 1
+                nums[j] |= x
+                j -= 1
+        return answer
+        
+        
