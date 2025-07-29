7be7b5c44bf843f662d19b01f06cedad42568c37
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-29 19:27:40 +0530

    Update .py

--- .py
+++ .py
@@ -1,3 +1,3 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        return [*map(lambda d:max(d.values())-d[-1]+1
+        return [*map(lambda d:max(d.values())-d[-1]+1,accumulate(range(len(a)-1,-1,-1),
