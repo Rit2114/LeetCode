0434b83b8b18086ba31acb2204571ba13d5e55b9
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-07-29 19:29:41 +0530

    Update .py

--- .py
+++ .py
@@ -2,3 +2,7 @@ class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
         n = len(nums)
         answer = [0] * n
+        for i in range(n):
+            x = nums[i]
+            answer[i] = 1
+            j = i - 1
