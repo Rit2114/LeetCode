b47064bdffe82f673d2df6c8ed7d526fe8093c04
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-08 03:37:15 +0530

    Update .py

--- .py
+++ .py
@@ -1,3 +1,9 @@
 class Solution:
     def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
-        
+        n=len(fruits)
+        diag=0
+        for i, row in enumerate(fruits):
+            diag+=row[i]
+        for i in range(n-2):
+            fruits[i][n-2-i]=fruits[i][n-3-i]=0
+        fruits[n-2][0]=0
