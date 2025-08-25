8eb8922d2cb664adeca3f2b7f6434558a7a7bb31
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-08-25 13:23:43 +0530

    Update 498. Diagonal Traverse.py

--- 498. Diagonal Traverse.py
+++ 498. Diagonal Traverse.py
@@ -1,4 +1,5 @@
 
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        
+        rows, cols = len(mat), len(mat[0])
+        diagonals = [[] for _ in range(rows + cols - 1)]
