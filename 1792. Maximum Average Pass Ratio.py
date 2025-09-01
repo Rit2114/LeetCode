45676107669b6a9acf385b78bda6bbe2d64f6028
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-01 21:14:16 +0530

    Update 1792. Maximum Average Pass Ratio.py

--- 1792. Maximum Average Pass Ratio.py
+++ 1792. Maximum Average Pass Ratio.py
@@ -1,3 +1,8 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        
+        n=len(classes)
+        sum=0
+        A=[]
+        for p,q in classes:
+            sum+=p/q
+            A.append(((p-q)/(q*(q+1)), p, q)) # change sign
