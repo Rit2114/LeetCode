cafd1d23d673028e1af4de5cc0ec892835a8906b
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-03 23:23:23 +0530

    Update 3027. Find the Number of Ways to Place People II.py

--- 3027. Find the Number of Ways to Place People II.py
+++ 3027. Find the Number of Ways to Place People II.py
@@ -1,3 +1,7 @@
 class Solution:
     def numberOfPairs(self, points: list[list[int]]) -> int:
+        points.sort(key=lambda x: (x[0], -x[1]))
+        n = len(points)
+        result = 0
+
         
