2be758d409197d76f6971c9ae64b6a7f0b96cc8f
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-05 17:36:50 +0530

    Update 2749. Minimum Operations to Make the Integer Zero.py

--- 2749. Minimum Operations to Make the Integer Zero.py
+++ 2749. Minimum Operations to Make the Integer Zero.py
@@ -3,4 +3,11 @@ class Solution:
         if num1 == 0:
             return 0
 
+        for t in range(0, 61):  # 0..60
+            s = num1 - t * num2
+            if s < 0:
+                continue
+            if s < t:
+                continue
+
       
