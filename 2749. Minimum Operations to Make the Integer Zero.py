85263f1c3263c6dbaa2d0cf1aa896e8202d7813e
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-05 17:37:16 +0530

    Update 2749. Minimum Operations to Make the Integer Zero.py

--- 2749. Minimum Operations to Make the Integer Zero.py
+++ 2749. Minimum Operations to Make the Integer Zero.py
@@ -9,5 +9,9 @@ class Solution:
                 continue
             if s < t:
                 continue
+            # Python 3.8+: bit_count gives number of set bits
+            ones = s.bit_count()
+            if ones <= t:
+                return t
 
       
