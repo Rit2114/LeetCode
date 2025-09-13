de7d7f5848b52989a13e061e021b84b7e877a6d7
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-09-13 18:13:19 +0530

    Update 3541. Find Most Frequent Vowel and Consonant.py

--- 3541. Find Most Frequent Vowel and Consonant.py
+++ 3541. Find Most Frequent Vowel and Consonant.py
@@ -3,3 +3,6 @@ class Solution:
         freq=Counter(s)
         maxCV=[0]*2
         for i, f in freq.items():
+            idx=((1<<(ord(i)-97)) & 0x104111)!=0
+            maxCV[idx]=max(maxCV[idx], f)
+        return maxCV[0]+maxCV[1]
