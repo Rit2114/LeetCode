2e37e187bfad336870b3fd3bc525c1d308b959fb
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-10-17 12:51:25 +0530

    Update Maximize the Number of Partitions After Operations.py

--- Maximize the Number of Partitions After Operations.py
+++ Maximize the Number of Partitions After Operations.py
@@ -27,5 +27,27 @@ class Solution(object):
             ansr[i] = ans
             usedr[i] = used
 
+            l = 0
+        while l < n:
+            used = 0
+            cntUsed = 0
+            usedBeforeLast = 0
+            usedTwiceBeforeLast = 0
+            last = -1
+            r = l
+
+            while r < n:
+                ch = ord(s[r]) - ord('a')
+                if (used & (1 << ch)) == 0:
+                    if cntUsed == k:
+                        break
+                    usedBeforeLast = used
+                    last = r
+                    used |= (1 << ch)
+                    cntUsed += 1
+                elif cntUsed < k:
+                    usedTwiceBeforeLast |= (1 << ch)
+                r += 1
+
         ansl = 0
         ans = ansr[0]
