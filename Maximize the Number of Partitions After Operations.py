b0c1e7980e2d3c5be4871e22d3b17802728e9438
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-10-17 12:48:59 +0530

    Update Maximize the Number of Partitions After Operations.py

--- Maximize the Number of Partitions After Operations.py
+++ Maximize the Number of Partitions After Operations.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
+        if k == self.ALPHABET_SIZE:
+            return 1
+
+        n = len(s)
+        ansr = [0] * n
+        usedr = [0] * n
+        used = 0
+        cntUsed = 0
+        ans = 1
