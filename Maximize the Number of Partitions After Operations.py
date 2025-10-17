d7029b5bfbddec4e32696b408d9d4129fd1a4231
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-10-17 12:52:29 +0530

    Update Maximize the Number of Partitions After Operations.py

--- Maximize the Number of Partitions After Operations.py
+++ Maximize the Number of Partitions After Operations.py
@@ -1,4 +1,6 @@
-class Solution(object):
+class Solution:
+    ALPHABET_SIZE = 26
+
     def maxPartitionsAfterOperations(self, s, k):
         """
         :type s: str
@@ -27,7 +29,10 @@ class Solution(object):
             ansr[i] = ans
             usedr[i] = used
 
-            l = 0
+        ansl = 0
+        ans = ansr[0]
+
+        l = 0
         while l < n:
             used = 0
             cntUsed = 0
@@ -49,5 +54,26 @@ class Solution(object):
                     usedTwiceBeforeLast |= (1 << ch)
                 r += 1
 
-        ansl = 0
-        ans = ansr[0]
+            if cntUsed == k:
+                if last - l > bin(usedBeforeLast).count('1'):
+                    ans = max(ans, ansl + 1 + ansr[last])
+                if last + 1 < r:
+                    if last + 2 >= n:
+                        ans = max(ans, ansl + 1 + 1)
+                    else:
+                        if bin(usedr[last + 2]).count('1') == k:
+                            canUse = ((1 << self.ALPHABET_SIZE) - 1) & ~used & ~usedr[last + 2]
+                            if canUse > 0:
+                                ans = max(ans, ansl + 1 + 1 + ansr[last + 2])
+                            else:
+                                ans = max(ans, ansl + 1 + ansr[last + 2])
+                            l1 = ord(s[last + 1]) - ord('a')
+                            if (usedTwiceBeforeLast & (1 << l1)) == 0:
+                                ans = max(ans, ansl + 1 + ansr[last + 1])
+                        else:
+                            ans = max(ans, ansl + 1 + ansr[last + 2])
+
+            l = r
+            ansl += 1
+
+        return ans
