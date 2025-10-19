0709db2ff43a92a42b19c938e58f5f91fae5a485
Rit Sarkar <159016930+Rit2114@users.noreply.github.com>
2025-10-19 17:56:08 +0530

    Update .py

--- .py
+++ .py
@@ -1,1 +1,8 @@
-
+lass Solution(object):
+    def findLexSmallestString(self, s, a, b):
+        """
+        :type s: str
+        :type a: int
+        :type b: int
+        :rtype: str
+        """
